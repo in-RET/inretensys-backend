3971e76d4d0f8ff6655443d9273bc591a1dc2514
Andreas Lubojanski <andreas.lubojanski@hs-nordhausen.de>
2023-07-26 08:45:04 +0200

    bug fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t inretensys:0.2a5-gurobi -f production/gurobi/dockerfile .
-docker build -t inretensys:0.2a5-cbc -f production/cbc/dockerfile .
+docker build -t inretensys:0.2a6-gurobi -f production/gurobi/dockerfile .
+docker build -t inretensys:0.2a6-cbc -f production/cbc/dockerfile .
