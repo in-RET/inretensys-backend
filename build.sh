fafbe8d871b4ac9f1b0ebbc5392685b7e4b9d3f4
pyrokar <andreas@lubojanski.com>
2023-10-16 10:35:45 +0200

    updated build-script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t inretensys:0.2a6-gurobi -f production/gurobi/dockerfile .
-docker build -t inretensys:0.2a6-cbc -f production/cbc/dockerfile .
+docker build -t inretensys:0.2a7-gurobi -f production/gurobi/dockerfile .
+docker build -t inretensys:0.2a7-cbc -f production/cbc/dockerfile .
